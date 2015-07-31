1ff8d0dcfbaf56bec192945acad773b7f37bdfd7
RUILIN CHU <rc200@balcony.orchestra>
2015-07-31 12:33:19 -0400

    jimi updated

---
+++ parallel_example6.m
@@ -0,0 +1,32 @@
+c=parallel.cluster.LSF();
+evalc('system(''mkdir -p ~/MATLAB_JOB_STORAGE'')');
+c.JobStorageLocation='~/MATLAB_JOB_STORAGE';
+c.SubmitArguments='-W 00:02 -q mpi -R centos ';
+c.parpool(22)
+
+spmd
+switch labindex 
+ case 1
+  A = 1:5;
+  labSend(A,2,199);
+ case 2
+  A = labReceive(1,199);
+ otherwise
+  A = 11:15;
+ end
+ system('hostname');
+end
+
+A{:}
+
+spmd
+if labindex == 1
+  A=labBroadcast(1,1:5);
+ else
+  A=labBroadcast(1);
+end
+end
+
+A{:}
+
+delete(gcp)
